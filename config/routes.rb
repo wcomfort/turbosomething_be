511ffcfeea81b98a9258deeeb3036887ff2ef9d3
Will <comfortwb21@gmail.com>
2019-11-08 12:59:54 -0500

    api set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :user_cars
-  resources :cars
-  resources :users
+  # resources :user_cars
+  resources :cars, only: [:index]
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
