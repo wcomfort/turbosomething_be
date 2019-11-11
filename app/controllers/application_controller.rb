8c03b6d863d63427fbbc8df5fb9f1cc8dd7305ac
Will <comfortwb21@gmail.com>
2019-11-11 17:50:39 -0500

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    def login
+        user = User.find_by(email: params[:email])
+        render json: user
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # resources :user_cars
   resources :cars, only: [:index, :create]
   resources :users, only: [:index]
+  post '/login', to: 'application#login'
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
