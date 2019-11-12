c12f000c92361a7957613abb43c5271a7bcb1ec3
Will <comfortwb21@gmail.com>
2019-11-12 14:49:40 -0500

    add favs

--- app/controllers/user_cars_controller.rb
+++ app/controllers/user_cars_controller.rb
@@ -1,2 +1,17 @@
 class UserCarsController < ApplicationController
+    def index
+        favs = UserCar.all 
+        render json: favs
+    end
+
+    def create
+        fav = UserCar.create(favparams)
+        render json: fav
+    end
+
+    private
+
+    def favparams
+        params.require(:user_car).permit(:user_id, :car_id)
+    end
 end
