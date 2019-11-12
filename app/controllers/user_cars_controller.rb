05810da09c3d0f928bb7d2846633ec207a4ed8b9
Will <comfortwb21@gmail.com>
2019-11-12 17:50:13 -0500

    delete favs

--- app/controllers/user_cars_controller.rb
+++ app/controllers/user_cars_controller.rb
@@ -1,12 +1,18 @@
 class UserCarsController < ApplicationController
     def index
         favs = UserCar.all 
-        render json: favs
+        render json: favs.to_json(:include => :car)
     end
 
     def create
         fav = UserCar.create(favparams)
         render json: fav
+    end
+
+    def destroy
+        fav = UserCar.find(params[:id])
+        fav.destroy
+        self.index 
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :cars, only: [:index, :create]
   resources :users, only: [:index]
-  resources :user_cars, only: [:index, :create]
+  resources :user_cars, only: [:index, :create, :destroy]
   post '/login', to: 'application#login'
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
