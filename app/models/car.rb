15696a58ba126e93b39f01d9f597b3fbf441f2c8
Will <comfortwb21@gmail.com>
2019-11-12 13:56:46 -0500

    AR validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,13 @@
 class Car < ApplicationRecord
     has_many :user_cars
     has_many :users, through: :user_cars
+
+    validates :picture, presence: true
+    validates :make, presence: true
+    validates :model, presence: true
+    validates :price, presence: true
+    validates :hp, presence: true
+    validates :tq, presence: true
+    validates :des, presence: true
+    validates :link, presence: true
 end
