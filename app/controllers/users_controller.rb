f6645f392e149a18c7ae05cfe46cf6ec418a0e66
Antonio Abraham <tony.calablava@gmail.com>
2019-11-12 14:20:36 -0500

    users_controller create method w/ userparams added, user create route added, user validations added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
-    def index
-        users = User.all 
-        render json: users 
-    end
-    
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = Car.create(userparams)
+    render json: user
+  end
+  private
+  def userparams
+    params.require(:user).permit(:firstName, :lastName, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # resources :user_cars
   resources :cars, only: [:index, :create]
-  resources :users, only: [:index]
+  resources :users, only: [:index, :create]
   post '/login', to: 'application#login'
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
